37294e9c035bf8362871ab9bc9f647aed26754c8
hhimanshu <harit.himanshu@yahoo.in>
2021-04-02 18:38:57 -0700

    Added styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
 
 type HelloType = {
     person: string;
 }
 
-export default ({ person }: HelloType) => <h1>Hello {person}</h1>
+export default ({ person }: HelloType) => <Title>Hello {person}</Title>
